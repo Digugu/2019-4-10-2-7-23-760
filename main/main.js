de6f744591778295ea446b3c6710f8e994b5012b
LiuJH <982070891@qq.com>
2019-05-31 16:48:27 +0800

    add file

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    let distanceCost = 6
+    if(inputs.distance > 2 && inputs.distance <= 8)
+        distanceCost = distanceCost + 0.8 * (inputs.distance - 2)
+    if(inputs.distance > 8)
+        distanceCost = distanceCost + 0.8 * (8 - 2) + 0.8 * (1 + 0.5) * (inputs.distance - 8)
+    let sum = distanceCost + 0.25 * inputs.parkTime
+    return Math.round(sum)
 };
